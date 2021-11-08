fb69899bd36a01603cb42bf7206945b2d301e65f
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-11-08 01:13:19 -0300

    Componente: Header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -37,7 +37,7 @@ export const GlobalStyle = createGlobalStyle`
     h1, h2, h3, h4, h5, h6, strong{
         font-weight: 600;
     }
-    buton{
+    button{
         cursor: pointer;
     }
     [disabled]{
