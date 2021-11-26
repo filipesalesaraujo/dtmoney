e7e3668334aa4b9a6e92190244706e2a9ba25985
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-11-26 19:35:04 -0300

    Formatando valores

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -5,15 +5,17 @@ import { Container } from "./styles";
 interface Transaction {
   id: number;
   title: string;
-  amout: number;
+  amount: number;
   type: string;
-  category: string; 
+  category: string;
   createdAt: string;
 }
 export function TransactionsTable() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   useEffect(() => {
-    api.get("transactions").then((response) => setTransactions(response.data.transactions));
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
   }, []);
   return (
     <Container>
@@ -30,9 +32,18 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amout}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
