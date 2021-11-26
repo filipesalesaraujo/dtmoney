59140499bbbc8ed44524d0bd440824f6afd78bd8
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-11-26 19:16:46 -0300

    Inserindo transação na API

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,32 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Desenvolvimento",
+          type: "deposit",
+          category: "Dev",
+          amount: 3000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1000,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
+      ]
+    });
+  },
   routes() {
     this.namespace = "api";
     this.get("/transactions", () => {
-      return this.schema.all("transactions");
+      return this.schema.all("transaction");
     });
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
